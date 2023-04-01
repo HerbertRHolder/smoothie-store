cb4fecf9ba46e6a4e657c9fb1fe0fc9f615a5717
Herb <herbrholder@gmail.com>
2023-04-01 18:15:47 -0500

    Add template: register

--- src/main/java/com/store/smoothies/controllers/CollectionController.java
+++ src/main/java/com/store/smoothies/controllers/CollectionController.java
@@ -1,17 +1,12 @@
 package com.store.smoothies.controllers;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class AboutController {
-
-        @GetMapping("/about")
-        public String displayAboutPage(Model model){
-            return "about";
-        }
-
-
-
+public class CollectionController {
+    @GetMapping("/collection")
+    public String displayCollectionPage(Model model){
+        return "collection";
+    }
 }
--- src/main/java/com/store/smoothies/controllers/LoginController.java
+++ src/main/java/com/store/smoothies/controllers/LoginController.java
@@ -1,4 +1,16 @@
 package com.store.smoothies.controllers;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LoginController {
-}
+
+    @GetMapping("/login")
+        public String displayLoginPage(){
+            return "login";
+        }
+    }
+
