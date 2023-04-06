5b1e25e4ba71534830d4a2c606c467ca1a0ca0a5
Herb <herbrholder@gmail.com>
2023-04-05 22:35:21 -0500

    Add: listing feature with thymeleaf

--- src/main/java/com/store/smoothies/controllers/Collection.java
+++ src/main/java/com/store/smoothies/controllers/Collection.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.store.smoothies.services.ProductService;
+import org.springframework.web.servlet.ModelAndView;
+
 import java.util.List;
 
 @Controller
@@ -19,10 +21,21 @@ public class Collection {
 //    public String displayCollectionPage(){
 //        return "collection";
 //    }
+
+//    @GetMapping("/collection")
+//    public String displayCollectionPage(Model model){
+//        List<Product> prd = productRepo.findAll();
+//        model.addAttribute("products", prd);
+//        return "collection";
+//    }
     @GetMapping("/collection")
-    public String displayCollectionPage(Model model){
-        List<Product> prd = productRepo.findAll();
-        model.addAttribute("products", prd);
-        return "collection";
+    public ModelAndView getAllProducts(){
+        ModelAndView mav = new ModelAndView("collection");
+//        List<Product> products = this.productRepo.findAll();
+//        for (int i =0;i< products.size();i++){
+//            products.get(i).getName();
+//        }
+        mav.addObject("products", this.productRepo.findAll());
+        return mav;
     }
 }
--- src/main/java/com/store/smoothies/models/Product.java
+++ src/main/java/com/store/smoothies/models/Product.java
@@ -17,7 +17,35 @@ public class Product {
     @JoinColumn(name = "Inventory_id")
     private Inventory inventory_id;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Inventory getInventory_id() {
+        return inventory_id;
+    }
+
+    public void setInventory_id(Inventory inventory_id) {
+        this.inventory_id = inventory_id;
+    }
 }
