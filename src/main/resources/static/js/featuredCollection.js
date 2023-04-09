e538f67a96c97eee1b7509221c7e9661a2cdc206
Herb <herbrholder@gmail.com>
2023-04-09 15:33:43 -0500

    Add: login styles

--- src/main/resources/static/js/featuredCollection.js
+++ src/main/resources/static/js/featuredCollection.js
@@ -16,54 +16,9 @@
     let index = 0;
     const images = document.querySelectorAll("[data-src]");
     const imgOptions = {};
-    let products = document.querySelectorAll(".product-view");
-    let firstBottle = products[0];
-    firstBottle.classList.add("solidImg");
-
-    products = Array.from(products);
-    let stateObjs = [];
-    for (let i = 0; i < products.length; i++) {
-        let stateObj = {
-            element: products[i],
-            isSelected: false,
-        };
-        stateObjs.push(stateObj);
-    }
 
 
 
-    let smoothies = [
-        {
-            src: "img/orange.png",
-            name: "Mango",
-            view: stateObjs[0],
-            link: "/collection/1",
-            ingredients: mango
-
-
-        },
-        {
-            src: "img/red.png",
-            name: "Get the Beets",
-            view: stateObjs[1],
-            link: "/collection/2",
-            ingredients: getTheBeets
-        },
-        {
-            src: "img/yellow.png",
-            name: "Pine Ginger",
-            view: stateObjs[2],
-            link: "/collection/3",
-            ingredients: pineGinger
-        },
-        {
-            src: "img/green.png",
-            name: "Green Machine",
-            view: stateObjs[3],
-            link: "/collection/4",
-            ingredients: greenMachine
-        },
-    ]; // smoothieImgs
     breadCrumb.setAttribute("href", "/collection/1");
     breadCrumb.innerText = `${smoothies[0].name}`;
 
@@ -87,10 +42,9 @@
         }, imgOptions);
     } // end of if
 
-
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++
     prev.addEventListener("click", () => {
         index -= 1;
-
         if (index < 0) {
             index = smoothies.length - 1;
             smoothies[0].view.element.classList.remove("solidImg");
@@ -102,8 +56,13 @@
         featureProduct.setAttribute("href", `${smoothies[index].link}`)
         breadCrumb.innerText = `${smoothies[index].name}`;
         benefitInfo.innerText = `${smoothies[index].ingredients}`
+        smoothieName.innerText = `${smoothies[index].name}`
+        smoothieBenefit.innerText = `${smoothies[index].benefit}`
+        ingredientItems.innerHTML = `${smoothies[index].ingredientsList}`
+
         smoothies[index].view.element.classList.add("solidImg");
     }); // prev listener
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++
 
     next.addEventListener("click", () => {
         index += 1;
@@ -121,6 +80,11 @@
         benefitInfo.innerText = `${smoothies[index].ingredients}`
         featureProduct.setAttribute("href", `${smoothies[index].link}`);
         breadCrumb.innerText = `${smoothies[index].name}`;
+        smoothieName.innerText = `${smoothies[index].name}`
+        smoothieBenefit.innerText = `${smoothies[index].benefit}`
+        ingredientItems.innerHTML = `${smoothies[index].ingredientsList}`
         smoothies[index].view.element.classList.add("solidImg");
     }); // next listener
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 })();
